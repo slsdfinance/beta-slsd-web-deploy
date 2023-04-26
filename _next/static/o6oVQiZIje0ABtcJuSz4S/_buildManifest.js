76c88f76a23d166977fac6841c0aec0b50fde926
Devslsddev <Devslsddev@users.noreply.github.com>
2023-04-26 08:58:44 +0000

    deploy: slsdfinance/slsd-web@b804b3a56478267168f76ea3d6dfc8bf02d19206

---
+++ _next/static/o6oVQiZIje0ABtcJuSz4S/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-eab5237f3079fdd9.js"],"/_error":["static/chunks/pages/_error-6b5795b36f30e67b.js"],"/farm":["static/chunks/pages/farm-486efd90e477fbc8.js"],sortedPages:["/","/_app","/_error","/farm"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
